9b90ee851c9aef2f6176ca6cc903c817e12999a3
Gustavo Henrique <gustavo@imaginative.company>
2022-07-09 14:48:38 -0300

    code 1

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function Content() {
-  // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
